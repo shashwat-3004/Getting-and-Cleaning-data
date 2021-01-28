c94d2cdaf81c6e2aef5798001dcfbbe2dee400e8
Shashwat Patel <62668967+shashwat-3004@users.noreply.github.com>
2021-01-28 20:27:49 +0530

    Added comments

--- run_analysis.r
+++ run_analysis.r
@@ -26,7 +26,7 @@ mergedSubject<-rbind(subject_train,subject_test)
 # Working on Feature dataset
 
 ####################################
-x<-grepl(pattern="-mean\\(\\)|-std\\(\\)",feature$V2)
+x<-grepl(pattern="-mean\\(\\)|-std\\(\\)",feature$V2)   # features with mean and standard deviation are extracted with this
 feature<-feature[x,]
 
 feature$V2<-gsub(pattern="\\(\\)",replacement ="",feature$V2)
@@ -35,12 +35,12 @@ feature$V2<-gsub(pattern="\\(\\)",replacement ="",feature$V2)
 # Conversion to tidy data
 ####################################
 
-mergedData<-mergedData[,feature$V1]
-colnames(mergedData)<-feature$V2
+mergedData<-mergedData[,feature$V1]      # In the dataset, only those column with mean and standard deviaiton measurement are extracted out.
+colnames(mergedData)<-feature$V2         # Column names are given
 ####################################
 # Replacing Label number with activity name
 ####################################
-library(tidyverse)
+library(tidyverse)                       # Used case_when function included in dplyr package (included in tidyverse) to replace activity ids with activity names.
 mergedLabel<-mergedLabel %>% mutate(V1=case_when(      
                                 V1==1 ~ "Walking",
                                 V1==2 ~ "WalkingUpstairs",
@@ -53,10 +53,10 @@ mergedLabel<-mergedLabel %>% mutate(V1=case_when(
 ####################################
 # Adding subject and activity to mergedData 
 ####################################
-colnames(mergedLabel)<-"Activity"
+colnames(mergedLabel)<-"Activity"                             
 colnames(mergedSubject)<-"SubjectNo"
-mergedData<-cbind(mergedLabel,mergedData)
-mergedData<-cbind(mergedSubject,mergedData)
+mergedData<-cbind(mergedLabel,mergedData)               
+mergedData<-cbind(mergedSubject,mergedData)              # after this, the tidy data set is obtained.
 ####################################
 # Tidy dataset obtained, writing the table as text form
 ####################################
@@ -66,13 +66,16 @@ write.table(mergedData,"merged_and_cleaned_data.txt")
 # Creating averaged tidy dataset
 
 ####################################
-
+## To create the averaged tidy data set, summarise_all function is used included in dplyr package, the below code first groups by subject and activity,
+## and calculates the mean of the variables present in all columns
 tidy_dataset<-mergedData%>% group_by(SubjectNo,Activity)%>% summarise_all((mean))
-write.table(tidy_dataset,file="averaged_tidyData.txt")
+write.table(tidy_dataset,file="averaged_tidyData.txt")   ## Averaged tidy data set is obtained,
+                                                         ## independent tidy data set with the average of each variable for each activity and each subject. 
 ####################################
 # Averaged tidy dataset 2.0 
 
-Value<-colnames(tidy_dataset)[3:68]
+Value<-colnames(tidy_dataset)[3:68]                      ## The same averaged tidy dataset as above but it is a gathered dataset. It can be seen in 
+                                                         ## another_averaged_tidyDataset.txt.
 second_tidy_dataset<-gather(tidy_dataset,key="Varaible",value=Value,3:68)
 write.table(second_tidy_dataset,file="another_averaged_tidyDataset.txt")
 
